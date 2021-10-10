633c044f8e3be73c5e5c1d05c9e057e01b39147d
Lukas Karlsson <admin@lukwam.dev>
2021-10-10 22:42:32 +0000

    UPdated cloudbuild.

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -1,9 +1,9 @@
 data "google_cloudfunctions_function" "slack" {
   name = "slack"
 }
-# data "google_cloudfunctions_function" "slack_redirect" {
-#   name = "slack_redirect"
-# }
+data "google_cloudfunctions_function" "slack_redirect" {
+  name = "slack_redirect"
+}
 data "google_cloudfunctions_function" "truism" {
   name = "truism"
 }
@@ -19,13 +19,13 @@ resource "google_cloudfunctions_function_iam_member" "slack_allusers" {
   member = "allUsers"
 }
 
-# resource "google_cloudfunctions_function_iam_member" "slack_redirect_allusers" {
-#   project = data.google_cloudfunctions_function.slack_redirect.project
-#   region = data.google_cloudfunctions_function.slack_redirect.region
-#   cloud_function = data.google_cloudfunctions_function.slack_redirect.name
-#   role = "roles/cloudfunctions.invoker"
-#   member = "allUsers"
-# }
+resource "google_cloudfunctions_function_iam_member" "slack_redirect_allusers" {
+  project = data.google_cloudfunctions_function.slack_redirect.project
+  region = data.google_cloudfunctions_function.slack_redirect.region
+  cloud_function = data.google_cloudfunctions_function.slack_redirect.name
+  role = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
 
 resource "google_cloudfunctions_function_iam_member" "truism_allusers" {
   project = data.google_cloudfunctions_function.truism.project
